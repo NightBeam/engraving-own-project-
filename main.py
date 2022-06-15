3440e3f34abadfd0de41a273f5a78dc3089bee57
Mathew <id.matrix.2006@gmail.com>
2022-06-16 02:46:51 +0400

    added style

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def mainPage():
+    if(request.args.get('type')):
+        print('good job')
     return render_template("main.html", title="engraving")
 
 @app.errorhandler(404)
