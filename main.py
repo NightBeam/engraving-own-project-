e83835e42a12881012f28f32625b105063073af8
Mathew <id.matrix.2006@gmail.com>
2022-07-05 17:10:02 +0400

    add normal header

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__)
 
+listOfWeapons = ['AK','M4','DEAGLE','SWD']
+
+@app.route('/main/<wp>')
+def mainPage(wp):
+    return render_template("main.html", title="engraving", item=wp)
+
 @app.route('/')
-def mainPage():
-    if(request.args.get('type')):
-        print('good job')
-    return render_template("main.html", title="engraving")
+def WeaponsChangerPage():
+    return render_template("weaponsChanger.html", listOfWeapons=listOfWeapons)
 
 @app.errorhandler(404)
 def showError(error):
     return "404"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
